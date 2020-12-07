54adc947cf9b56111d5feddd81fd8fcd99201a0d
Max Base <MaxBaseCode@Gmail.Com>
2020-12-07 19:36:50 +0330

    Adding support https/WSS

--- server/serve.js
+++ server/serve.js
@@ -6,9 +6,54 @@
 */
 
 // import libs
-var app = require('express')()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+// var app = require('express')()
+// var http = require('http').Server(app)
+// var io = require('socket.io')(http)
+const fs          = require("fs");
+const ioServer    = require("socket.io");
+const http        = require("http")
+const https       = require("https");
+const httpPort    = 9098;
+const httpsPort   = 9099;
+
+// headers
+const CORS = (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    if(req.method === "OPTIONS" ) {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+};
+
+// http webserver
+const httpServer  = http.createServer(CORS);
+// https webserver
+const httpsServer = https.createServer({
+  // read certificates from disk
+  "key": fs.readFileSync("selfsigned.key"),
+  "cert": fs.readFileSync("selfsigned.crt"),
+  "ca": fs.readFileSync("selfsigned.ca")
+}, CORS);
+
+
+// socket
+const io = ioServer();
+
+// listen port(s)
+httpServer.listen(httpPort, function() {
+    console.log(`Listening HTTP on ${httpPort}`);
+});
+httpsServer.listen(httpsPort, function() {
+    console.log(`Listening HTTPS on ${httpsPort}`);
+});
+
+// attach socket to webserver(s)
+io.attach(httpServer);
+io.attach(httpsServer);
 
 members = []
 io.on('connection', function(socket) {
@@ -19,7 +64,8 @@ io.on('connection', function(socket) {
 
     if(members.indexOf(data) > -1) {
       socket.emit('user-exists', data + ' username is taken!')
-    } else {
+    }
+    else {
       members.push(data)
       socket.emit('user-set', {username: data})
     }
@@ -30,19 +76,3 @@ io.on('connection', function(socket) {
   })
 })
 
-// serve client page in home of server port
-// app.get('/', function(req, res) {
-//   res.sendfile('index.html')
-// })
-
-// allow x-requests
-app.all('/', function (request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*")
-  response.header("Access-Control-Allow-Headers", "X-Requested-With")
-  next()
-})
-
-// listen port
-http.listen(3003, function() {
-  console.log('listening on localhost:3003')
-})
