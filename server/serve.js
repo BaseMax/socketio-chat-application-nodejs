48444411bdab5c3eb89e54e7cff92bf7f4210626
Max Base <MaxBaseCode@Gmail.Com>
2020-12-07 16:20:41 +0330

    add io connection

--- server/serve.js
+++ server/serve.js
@@ -9,3 +9,23 @@
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+
+members = []
+io.on('connection', function(socket) {
+  console.log('A user connected')
+
+  socket.on('set-username', function(data) {
+    console.log(data)
+
+    if(members.indexOf(data) > -1) {
+      socket.emit('user-exists', data + ' username is taken!')
+    } else {
+      members.push(data)
+      socket.emit('user-set', {username: data})
+    }
+  })
+
+  socket.on('message', function(data) {
+    io.sockets.emit('message', data)
+  })
+})
