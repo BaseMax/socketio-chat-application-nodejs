b34c967e6b30c525bc94d8abe58f977d1f12077e
Max Base <MaxBaseCode@Gmail.Com>
2020-12-07 16:20:28 +0330

    add libs

--- server/serve.js
+++ server/serve.js
@@ -2,5 +2,10 @@
  * @Name: socketio-chat-application-nodejs
  * @Date: 2020-12-07
  * @Author: Max Base
- * @Repository: https://github.com/BaseMax/socketio-chat-application-nodejs/blob/main/server/serve.js
+ * @Repository: https://github.com/BaseMax/socketio-chat-application-nodejs
 */
+
+// import libs
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
